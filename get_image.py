f9db34ed0987b71840725a1d8484b0913547f3e3
Keiichi Watanabe <udon.watanabe@gmail.com>
2015-05-05 06:10:13 +0900

    cut out faces by opencv

--- get_image.py
+++ get_image.py
@@ -3,6 +3,7 @@
 import urllib2
 import wget
 from bs4 import BeautifulSoup
+import cv2
 
 def get_name(tag):
     divs = tag.find_all("div")
@@ -40,9 +41,39 @@ def get_image_from_page(page, f):
             m_id = str(memnum).zfill(3)
             name    = get_name(tag)
             img_url = get_image_url(tag)
-            img = wget.download(img_url, out="./img/"+m_id+".jpg")
+            img_path="./img/"+m_id+".jpg"
+            wget.download(img_url, out=img_path)
+            get_face(m_id)
             print m_id,": ",name
             print >>f, m_id,": ",name
+
+
+def get_face(m_id):
+    img_path="./img/"+m_id+".jpg"
+    cascade_path = "./haarcascade_frontalface_alt.xml"
+
+    image = cv2.imread(img_path)
+    image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+
+    cascade = cv2.CascadeClassifier(cascade_path)
+
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+
+    if len(facerect) == 1:
+        rect = facerect[0]
+
+        x = rect[0]
+        y = rect[1]
+        w = rect[2]
+        h = rect[3]
+
+        face = cv2.resize(image_gray[y: y + h, x: x + w], (64, 64))
+        face_path="./img/"+m_id+"_face.jpg"
+        cv2.imwrite(face_path, face)
+        return True
+    else:
+        print "fail!!"
+        return False
 
 
 def main():
--- similarity.py
+++ similarity.py
@@ -1,51 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import numpy
+import cv2
 
 MEMBER_NUM = 717
-img_vecs = []
+img_vecs = {}
 
 
-def read_img(img_id):
+def load_img(img_id):
     global img_vecs
-    img = "./img/"+str(img_id).zfill(3)+".pgm"
-    with open(img) as f:
-        data = f.read().split()
-        max_v = int(data[3])+0.0
-        data = data[4:] # remove header
-        vec = [int(v)/max_v for v in data]
-        assert(len(vec)==4096)
-        arr = numpy.array(vec)
-        return arr / numpy.linalg.norm(arr)
+    img_path = "./img/"+str(img_id).zfill(3)+"_face.jpg"
+    try:
+        image = cv2.imread(img_path, 0).flatten()
+        n_image = image / numpy.linalg.norm(image)
+        img_vecs[img_id] = n_image
+    except:
+        # 顔が認識されなかった
+        pass
 
-def calc_sim(a, b):
-    v1 = img_vecs[a]
-    v2 = img_vecs[b]
+def calc_sim(v1, v2):
     x = numpy.dot(v1, v2)
     y = numpy.linalg.norm(v1) * numpy.linalg.norm(v2)
     return x / y
 
 def main():
     global img_vecs
-    img_vecs = [read_img(i+1) for i in range(MEMBER_NUM)]
-
-    cvec = []
 
     for i in range(MEMBER_NUM):
+        load_img(i+1)
+
+    mx_v = 0
+    mx_pair = (-1, -1)
+    print " id| nearest| similarity"
+    for k1, v1 in img_vecs.items():
         c_max = 0
-        p = -1
-        for j in range(MEMBER_NUM):
-            if i==j:
+        p = 0
+        for k2, v2 in img_vecs.items():
+            if k1 == k2:
                 continue
-            c = calc_sim(i, j)
+            c = calc_sim(v1, v2)
             if c > c_max:
                 c_max = c
-                p = j
-        print i+1, p+1, c_max
-    cvec.sort()
-    print cvec[:5]
-    print "-------------"
-    print cvec[-5:]
+                p = k2
+        print str(k1).zfill(3), str(p).zfill(3), c_max
+        if c_max > mx_v:
+            mx_v = c_max
+            mx_pair = (k1, p)
+    print mx_v, mx_pair
 
 if __name__ == '__main__':
     main()
